f5ffd21570c464a3f5441f75db403a2827008230
Annalee Engsted <annalee.engsted@hotmail.com>
2019-10-08 17:52:29 +0200

    Master updated, subscription page styled, buttons work until step_delivery: ready for merging

--- script.js
+++ script.js
@@ -28,3 +28,48 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " w3-white";
 }
+
+// ANNALEES DEL:
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  document.querySelector("body").addEventListener("click", listClickController);
+}
+
+function listClickController(event) {
+  // controller for clicks
+  let element = event.target;
+
+  if (element.getAttribute("data-action") == "cta") {
+    showStepSize();
+  } else if (element.getAttribute("data-action") == "show_subs") {
+    showSubs();
+  }
+}
+
+function showStepSize() {
+  console.log("showStepSize logged");
+  document.querySelector(".basket_bg").style.display = "block";
+  // document.querySelector(".basket_wrapper").style.bottom = "0";
+  document.querySelector(".basket_wrapper").classList.add("slide");
+  document.querySelector(".basket_wrapper").addEventListener("animationend", () => {
+    document.querySelector(".basket_wrapper").style.bottom = "0";
+  });
+}
+
+function showSubs() {
+  console.log("showSubs logged");
+  document.querySelector("#step_size").style.display = "none";
+  document.querySelector("#step_subscription").style.display = "grid";
+
+  document.querySelectorAll(".plan").forEach(plan => {
+    plan.addEventListener("click", function() {
+      console.log(this);
+      document.querySelectorAll(".plan").forEach(plan => {
+        plan.style.border = "2px groove var(--color-bggray)";
+      });
+      this.style.border = "2px groove var(--color-purple)";
+    });
+  });
+}
